364cf177205ab2fd4e21d54a984c747d62c03d94
Muhammad Umar Farooq <umar.malik017@gmail.com>
2024-03-30 13:04:09 +0500

    calculator Project
    Project Calculator

---
+++ index.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+
+    const answers : {
+        numberOne: number,
+        numberTwo: number,
+        operator: string 
+} = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Please enter your first number: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Please enter your Second number: " 
+    }, 
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+","-", "/"],
+        message: "Select Operator: "
+    },
+    ]);
+    // console.log(answers)
+    const {numberOne, numberTwo, operator} = answers;
+    if (numberOne && numberTwo && operator){
+        let result: number = 0;
+            if (operator === "+"){
+            result = numberOne + numberTwo
+        }   if (operator === "-"){
+            result = numberOne - numberTwo
+        }   if (operator === "*"){
+            result = numberOne * numberTwo
+        }   if (operator === "/"){
+            result = numberOne / numberTwo
+        }
+        console.log("The result be : ",result)
+    } else {
+        console.log("Please enter valid input")
+    }
